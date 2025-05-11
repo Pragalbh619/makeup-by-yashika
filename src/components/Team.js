655764d45b9e2cee3b2aa69e8141d269c2389098
Pragalbh Kumar Srivastava <pragalbhkumars@gmail.com>
2025-05-11 18:59:02 +0530

    changes in team

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,23 +7,35 @@ const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Team</h2>
           <p>
-            Our expert makeup artists are dedicated to enhancing your beauty with perfection.
+            Our expert makeup artists are dedicated to enhancing your beauty
+            with perfection.
           </p>
         </div>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
-              <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                <div className="thumbnail">
-                  {" "}
-                  <img src={d.img} alt="..." className="team-img" />
-                  <div className="caption">
-                    <h4>{d.name}</h4>
-                    <p>{d.job}</p>
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    {/* <img src={d.img} alt="..." className="team-img" /> */}
+                    <img
+                      src={d.img}
+                      alt="..."
+                      className="team-img"
+                      style={{
+                        height: "250px",
+                        width: "100%",
+                        objectFit: "cover",
+                        borderRadius: "8px",
+                      }}
+                    />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
+              ))
             : "loading"}
         </div>
       </div>
@@ -31,5 +43,4 @@ const Team = (props) => {
   );
 };
 
-
-export default Team
+export default Team;
